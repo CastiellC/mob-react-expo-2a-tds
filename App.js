b75722ad315627f9f69ac89680f1e9924076e3c9
Carlos <160151768+CastiellC@users.noreply.github.com>
2024-10-31 12:10:29 -0300

    carga_
     carga_

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Cesta />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
